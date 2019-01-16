d4b66dce4f44793b527109b1fa0fef6c367c6256
Pulpaul <43722895+Pulpaul@users.noreply.github.com>
2019-01-16 10:07:09 +0800

    Update database_connection.php

--- php/database_connection.php
+++ php/database_connection.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(0);
-$connect = new PDO('mysql:host=localhost;dbname=dbcits', 'root', ''); 
+//$connect = new PDO('mysql:host=ec2-54-235-68-3.compute-1.amazonaws.com;dbname=d2a4hhgqv2n24f', 'uqvjzapylrjlbg', 'b60f4ffd3a595b653319f3f21c83dc877646d72fca07b9e3eafaf604888313ec');
+
+$connect = new PDO('mysql:host=localhost;dbname=dbcits', 'root', '');
 session_start(); 
 
 ?> 
